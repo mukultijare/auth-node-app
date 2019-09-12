7eb04b9f3b15bf338717be6d0bb9b205c1486275
unknown <mukultijare@gmail.com>
2019-09-12 19:28:37 +0530

    erwe

--- app.js
+++ app.js
@@ -2,21 +2,17 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     
     app = express();
-    
-var port = process.env.PORT || 3000;
 
+var logFmt = require("logfmt");
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-var pg = require('pg');  
-      
-
+var pg = require('pg');
 var conString = process.env.DATABASE_URL || "postgres://ucbzlrpomwzmlg:899fc4bcde05cead89e80a824437956c7aa8696238ad6d6887edffab3352ae14@ec2-174-129-227-51.compute-1.amazonaws.com:5432/dd4fv7nr6k8v95";
 var client = new pg.Client(conString);
 
 client.connect();   
-
 
 app.get('/' , function(req,res) {
     res.sendfile(__dirname + 'index.html');
@@ -39,7 +35,8 @@ app.get('/logout', function(request, response) {
     
 });
 
-
-app.listen(port, function () {
-    console.log('Express server listening on port');
+app.set('port', process.env.PORT || 3001);
+app.use(errorHandler());
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
