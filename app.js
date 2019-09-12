752f52b7b3a79bc8d34161877a0fdba01c5f79bb
unknown <mukultijare@gmail.com>
2019-09-12 22:31:09 +0530

    erwe

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/login', function(request, response){
 app.post('/auth', function(request, response) { 
     var username = request.body.uname;
     var password = request.body.upwd;
-    response.redirect('/'+username);
+    response.redirect('/'+username.length);
     /*if (username && password) 
     {
         //response.redirect('/home');
