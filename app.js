30a449fac5a1b0d67c3b9d885344653e0f5f26da
unknown <mukultijare@gmail.com>
2019-09-12 20:20:10 +0530

    erwe

--- app.js
+++ app.js
@@ -3,26 +3,44 @@ var express = require('express'),
     errorHandler = require('express-error-handler'),    
     app = express();
 
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
+//Connection with Postgres Database
 var pg = require('pg');
 var conString = process.env.DATABASE_URL || "postgres://ucbzlrpomwzmlg:899fc4bcde05cead89e80a824437956c7aa8696238ad6d6887edffab3352ae14@ec2-174-129-227-51.compute-1.amazonaws.com:5432/dd4fv7nr6k8v95";
 var client = new pg.Client(conString);
+client.connect(); 
 
-client.connect();   
+//Session Handler
+const session = require('express-session');
+app.use(session({
+	secret: 'secret',
+	resave: true,
+    saveUninitialized: true,
+    cookie: {
+        expires: 60000
+    }
+}));
 
 app.get('/' , function(req,res) {
     res.sendfile(__dirname + '/index.html');
 }); 
 
 app.post('/auth', function(request, response) { 
-    var username = request.body.user;
+    var username = request.body.uname;
 	var password = request.body.upwd;
-    if (username == "Mukul") 
+    if (username && password) 
     {
+
+        
 		response.redirect('/dash');
 	}
+});
+
+app.get('/dash', function(request, response) {
+    response.sendfile(__dirname + '/signup.html');
 });
 
 app.get('/dash', function(request, response) {
