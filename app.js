35df514e24db20d7bbb1fe6bca738fdce6c26462
unknown <mukultijare@gmail.com>
2019-09-13 09:00:42 +0530

    aswe

--- app.js
+++ app.js
@@ -10,18 +10,22 @@ app.use(bodyParser.json());
 var pg = require('pg');
 var conString = process.env.DATABASE_URL || "postgres://ucbzlrpomwzmlg:899fc4bcde05cead89e80a824437956c7aa8696238ad6d6887edffab3352ae14@ec2-174-129-227-51.compute-1.amazonaws.com:5432/dd4fv7nr6k8v95";
 var client = new pg.Client(conString);
-client.connect(); 
+client.connect();
+//------------------------------------
 
 //Session Handler
 const session = require('express-session');
 app.use(session({
-	secret: 'secret',
+    secret: 'secret',
+    name: 'farmerApp',
 	resave: true,
     saveUninitialized: true,
     cookie: {
         expires: 60000
     }
 }));
+//--------------------------
+
 
 app.get('/' , function(request,response) {
     response.redirect('/login');
